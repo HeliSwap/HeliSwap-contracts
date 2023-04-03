7b1ba53d650e0223b46bafbe982d9f4741f3b20f
Martin Dobrev <martindobrev87@gmail.com>
2023-04-03 16:27:09 +0300

    Migration contract updated WIP

--- contracts/periphery/Migration.sol
+++ contracts/periphery/Migration.sol
@@ -28,10 +28,14 @@ contract Migration {
         // Transfer LP tokens to migration contract
         address pair = UniswapV2Library.pairFor(
             oldFactory,
-            _tokenAddress,
-            _whbar
+            _whbar,
+            _tokenAddress
         );
         IERC20(pair).transferFrom(msg.sender, address(this), _liquidity);
+
+        IERC20(pair).approve(address(oldRouter), _liquidity);
+
+        bool isHTS = optimisticAssociation(_tokenAddress);
 
         // Remove liquidity from old pool/router
         (uint amountToken, uint amountHBAR) = oldRouter.removeLiquidityHBAR(
@@ -43,18 +47,22 @@ contract Migration {
             _deadline
         );
 
-        // Give approval to new router to spend tokens
-        IERC20(_tokenAddress).approve(address(newRouter), amountToken);
+        // // Give approval to new router to spend tokens
+        // IERC20(_tokenAddress).approve(address(newRouter), amountToken);
 
-        // Provide native liquidity to new pool assuming the ratios are the same?
-        newRouter.addLiquidityHBAR{value: amountHBAR}(
-            _tokenAddress,
-            amountToken,
-            _amoutAMin,
-            amountHBAR,
-            _userAddress,
-            _deadline
-        );
+        // // Provide native liquidity to new pool assuming the ratios are the same?
+        // newRouter.addLiquidityHBAR{value: amountHBAR}(
+        //     _tokenAddress,
+        //     amountToken,
+        //     _amoutAMin,
+        //     amountHBAR,
+        //     _userAddress,
+        //     _deadline
+        // );
+
+        if (isHTS) {
+            dissociate(_tokenAddress);
+        }
     }
 
     // Write similar function for non native liquidity provison
@@ -96,5 +104,38 @@ contract Migration {
             msg.sender,
             _deadline
         );
+    }
+
+    // calls HTS precompile in order to execute optimistic association
+    function optimisticAssociation(address token) internal returns (bool) {
+        (bool success, bytes memory result) = address(0x167).call(
+            abi.encodeWithSignature(
+                "associateToken(address,address)",
+                address(this),
+                token
+            )
+        );
+        require(success, "HTS Precompile: CALL_EXCEPTION");
+        int32 responseCode = abi.decode(result, (int32));
+        // Success = 22; Non-HTS token (erc20) = 167
+        require(
+            responseCode == 22 || responseCode == 167,
+            "HTS Precompile: CALL_ERROR"
+        );
+        return responseCode == 22;
+    }
+
+    // calls HTS precompile in order to execute token dissociation
+    function dissociate(address token) internal {
+        (bool success, bytes memory result) = address(0x167).call(
+            abi.encodeWithSignature(
+                "dissociateToken(address,address)",
+                address(this),
+                token
+            )
+        );
+        require(success, "HTS Precompile: CALL_EXCEPTION");
+        int32 responseCode = abi.decode(result, (int32));
+        require(responseCode == 22);
     }
 }
